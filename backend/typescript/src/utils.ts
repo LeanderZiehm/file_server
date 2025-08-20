f810540907092257e32bb306a4ad2f74cb4f966b
Leander <leander-arun.ziehm@stud.th-deg.de>
2025-08-21 00:14:20 +0200

    Chosen Stack: React and Go

--- backend/typescript/src/utils.ts
+++
@@ -1,6 +0,0 @@
-import crypto from "crypto";
-
-export function generateFileId(filename: string): string {
-  const timestamp = Date.now();
-  return crypto.createHash("sha256").update(filename + timestamp).digest("hex");
-}
